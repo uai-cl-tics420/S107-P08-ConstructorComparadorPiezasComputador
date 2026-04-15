2ae1d7c9dcaabd6545a065748bafd3eb3148dc86
Philippe-Pefaur <phpefaur@gmail.com>
2026-04-15 17:27:17 -0400

    Modificado frontend.tsx para admitir la carga de multiples vistas según ruta, para ello, modificado estructura de src y creado versiones en blanco de vista para log in.

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -5,16 +5,25 @@
  * It is included in `src/index.html`.
  */
 
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { App } from '@/views/App';
+import { LogIn } from '@/views/LogIn';
 
 function start() {
-  const root = createRoot(document.getElementById("root")!);
-  root.render(<App />);
+  const root = createRoot(document.getElementById('root')!);
+  root.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/login' element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>,
+  );
 }
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", start);
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', start);
 } else {
   start();
 }
---
+++ src/views/LogIn.tsx
@@ -0,0 +1,6 @@
+import { LogInSignOn } from '@/components/LogIn/LogIn_SignOn';
+import '@/index.css';
+
+export function LogIn() {
+  return <LogInSignOn />;
+}
